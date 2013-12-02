025b6f9b4b68174b87177d2c70caf19253677116
jialiya <jialiya.huang@students.olin.edu>
2013-12-02 14:34:43 -0500

    fixing heartbeat

--- heartbeat.sh
+++ heartbeat.sh
@@ -1,8 +1,9 @@
 # on startup always ping testalator web
+cd ~/testalator-device
 ip=$(ifconfig | awk -F':' '/inet addr/&&!/127.0.0.1/{split($2,_," ");print _[1]}')
 gateway=$(/sbin/ip route | awk '/default/ { print $3 }')
 build=$(git rev-parse HEAD)
 
 # check out what this bench's device name is
-echo '{"device":"'$DEVICE'", "ip":"'$ip'"}'
+echo '{"device":"'$DEVICE'", "ip":"'$ip'", "build":"'$build'", "gateway":"'$gateway'"}'
 curl -H 'Content-Type: application/json' -d '{"name":"'$DEVICE'", "ip":"'$ip'", "gateway":"'$gateway'", "build": "'$build'"}' testalator.herokuapp.com/bench
