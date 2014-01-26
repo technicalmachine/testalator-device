98c1084fe0bf51da132a8203c265919beddd7479
jia huang <jia@technical.io>
2014-01-26 15:24:00 -0500

    awking

--- heartbeat.sh
+++ heartbeat.sh
@@ -2,7 +2,7 @@
 # on startup always ping testalator web
 #. /home/pi/.bashrc
 cd /home/pi/testalator-device
-device=$(more device)
+device=$(awk '1' device)
 ip=$(/sbin/ifconfig | awk -F':' '/inet addr/&&!/127.0.0.1/{split($2,_," ");print _[1]}')
 gateway=$(/sbin/ip route | awk '/default/ { print $3 }')
 build=$(git rev-parse HEAD)
