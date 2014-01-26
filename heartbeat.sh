0e0bcafd6a76ee0098ed405836e981958387b136
jia huang <jia@technical.io>
2014-01-26 14:49:36 -0500

    ugh, screw profiles

--- heartbeat.sh
+++ heartbeat.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 # on startup always ping testalator web
-. $HOME/.bashrc
-cd ~/testalator-device
+#. /home/pi/.bashrc
+cd /home/pi/testalator-device
+device=$(more device)
 ip=$(/sbin/ifconfig | awk -F':' '/inet addr/&&!/127.0.0.1/{split($2,_," ");print _[1]}')
 gateway=$(/sbin/ip route | awk '/default/ { print $3 }')
 build=$(git rev-parse HEAD)
 
 # check out what this bench's device name is
-echo '{"device":"'$DEVICE'", "ip":"'$ip'", "build":"'$build'", "gateway":"'$gateway'"}'
-curl -H 'Content-Type: application/json' -d '{"name":"'$DEVICE'", "ip":"'$ip'", "gateway":"'$gateway'", "build": "'$build'"}' testalator.herokuapp.com/bench
+echo '{"device":"'$device'", "ip":"'$ip'", "build":"'$build'", "gateway":"'$gateway'"}'
+curl -H 'Content-Type: application/json' -d '{"name":"'$device'", "ip":"'$ip'", "gateway":"'$gateway'", "build": "'$build'"}' testalator.herokuapp.com/bench
