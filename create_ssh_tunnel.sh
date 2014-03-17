39476fba1b28b5e96d92b62bdd5879113f453614
jialiya <jialiya.huang@students.olin.edu>
2014-03-17 18:03:17 -0400

    tunnel forever

---
+++ create_ssh_tunnel.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+createTunnel() {
+  ip=$(awk -F"=" '/ip/{print $2}' device)
+  port=$(awk -F"=" '/port/{print $2}' device)
+  /usr/bin/ssh -N -R $port:localhost:22 root@ip
+  if [[ $? -eq 0 ]]; then
+    echo Tunnel to jumpbox created successfully
+  else
+    echo An error occurred creating a tunnel to jumpbox. RC was $?
+  fi
+}
+/bin/pidof ssh
+if [[ $? -ne 0 ]]; then
+  echo Creating new tunnel connection
+  createTunnel
+fi
