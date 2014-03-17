74a14f1c7a04f5e3001e55b60b77aba79ee33e90
jialiya <jialiya.huang@students.olin.edu>
2014-03-17 18:18:06 -0400

    adding ssh config

--- heartbeat.sh
+++ heartbeat.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 # on startup always ping testalator web
 #. /home/pi/.bashrc
-cd /home/pi/testalator-device
-device=$(awk '1' device)
+cd ~/testalator-device
+device=$(awk -F"=" '/device/{print $2}' device)
+ssh_ip=$(awk -F"=" '/ip/{print $2}' device)
+port=$(awk -F"=" '/port/{print $2}' device)
 ip=$(/sbin/ifconfig | awk -F':' '/inet addr/&&!/127.0.0.1/{split($2,_," ");print _[1]}')
 gateway=$(/sbin/ip route | awk '/default/ { print $3 }')
 build=$(git rev-parse HEAD)
 
 # check out what this bench's device name is
 echo '{"device":"'$device'", "ip":"'$ip'", "build":"'$build'", "gateway":"'$gateway'"}'
-curl -H 'Content-Type: application/json' -d '{"name":"'$device'", "ip":"'$ip'", "gateway":"'$gateway'", "build": "'$build'"}' testalator.herokuapp.com/bench
+curl -H 'Content-Type: application/json' -d '{"name":"'$device'", "ip":"'$ip'", "gateway":"'$gateway'", "build": "'$build'", "ssh":"'$ssh_ip'", "port":"'$port'"}' testalator.herokuapp.com/bench
